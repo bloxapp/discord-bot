e8ccaa7936ee8ddadf9642d83a270a3aff06cf5f
Vadim Chumak <vadim.inc@gmail.com>
2021-01-13 09:23:57 +0100

    move back to 8am periodic task

--- boundaries/bot.js
+++ boundaries/bot.js
@@ -20,7 +20,7 @@ const onReady = async () => {
   console.info(`Logged in as ${bot.user.username}!`);
   cron.schedule(`*/${validatorsPeriodMin} * * * *`, async() => emitMessage(await loadNewValidators('pyrmont', validatorsPeriodMin)));
   cron.schedule(`*/${validatorsPeriodMin} * * * *`, async() => emitMessage(await loadNewValidators('mainnet', validatorsPeriodMin)));
-  cron.schedule('20 1,20 * * *', async() => {
+  cron.schedule('0 8,20 * * *', async() => {
     emitMessage(await loadProcessStatistics());
     emitMessage(await loadRate('pyrmont'));
     emitMessage(await loadEff('pyrmont'));
